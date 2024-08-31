d154d97fd9905de2f9be1f47cf53db83af36a9d7
sandeep <sandeepcheriyan@gmail.com>
2024-08-31 09:38:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Landing from './pages/Landing'
+
+function App() {
+  return (
+   
+     <div className='w-full h-screen bg-red-500'>
+       <Landing/>
+     </div>
+  
+  )
+}
+
+export default App
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+const Landing= ()=>{
+    return(
+        <div className="bg-red-500 w-[100%] h-screen border " >
+            landing Page
+        </div>
+    )
+}
+
+export default  Landing 
