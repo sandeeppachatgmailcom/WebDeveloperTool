1ad38912a5a0885fb97ad091ac113ffbcc60d742
sandeep <sandeepcheriyan@gmail.com>
2024-09-02 19:40:36 +0530

    landing page animation direction modified

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -11,34 +11,23 @@ const Landing = () => {
     }
 
     return (
-        <div className="  w-full flex h-[100%]   " >
-            <div className="relative w-full overflow-hidden lg:w-3/4 h-full border justify-center flex  ">
+        <div className="relative  w-full flex h-[100%]   " >
+            <div className=" w-full overflow-hidden lg:w-3/4 h-full border justify-between  flex   ">
 
-                <div className="w-4/12 text-9xl h-screen border-white border-opacity-50 m-1 font-serif justify-center flex items-center  bg-gray-600 text-pink-500  motion-animation-down">
-                 <img
-                        className="w-full h-screen object-cover"
-                        src="https://assets.squarespace.com/static-images/conversion/left-column-a54fc58e08.webp"
-                        alt="Description of image"
-                    />
+                <div style={{backgroundImage:`url('https://assets.squarespace.com/static-images/conversion/left-column-a54fc58e08.webp')`,backgroundSize:'cover',backgroundPosition:'center'}} className="w-4/12 text-9xl  h-screen border-white border-opacity-50 m-1 font-serif justify-center flex items-center -rotate-45 bg-gray-600 text-pink-500  motion-animation-down">
+                  
                 </div>
-                <div className="w-4/12 text-9xl h-screen border-white border-opacity-50 m-1 font-serif justify-center flex items-center  bg-gray-600 text-pink-500  motion-animation-down">
-                 <img
-                        className="w-full h-screen object-cover"
-                        src="https://assets.squarespace.com/static-images/conversion/middle-column-736c47e154.webp"
-                        alt="Description of image"
-                    />
+                <div style={{backgroundImage:`url('https://assets.squarespace.com/static-images/conversion/middle-column-736c47e154.webp')`,backgroundSize:'cover',backgroundPosition:'center'}} className="w-4/12 text-9xl h-screen border-white border-opacity-50 m-1 font-serif justify-center flex items-center -rotate-45  bg-gray-600 text-pink-500  motion-animation-up">
+                  
                 </div>
-                <div className="w-4/12 text-9xl h-screen border-white border-opacity-50 m-1 font-serif justify-center flex items-center  bg-gray-600 text-pink-500  motion-animation-down">
-                 <img
-                        className="w-full h-screen object-cover"
-                        src="https://assets.squarespace.com/static-images/conversion/right-column-26c862b8dd.webp"
-                        alt="Description of image"
-                    />
+                <div style={{backgroundImage:`url('https://assets.squarespace.com/static-images/conversion/right-column-26c862b8dd.webp')`,backgroundSize:'cover',backgroundPosition:'center'}} className="w-4/12 text-9xl h-screen border-white border-opacity-50 m-1 font-serif justify-center flex items-center -rotate-45 bg-gray-600 text-pink-500  motion-animation-down">
+                 
                 </div>
                 
 
-                <div className=" w-full h-full absolute"></div>
+                
             </div>
+            <div className=" w-full h-full absolute"></div>
             <div className="w-full bg-black lg:w-1/4 flex relative h-[100%] ">
                 <div onClick={handleStart} className="absolute w-3/6 h-[10%] border justify-start cursor-pointer items-center border-white bottom-32 start-10 flex  " >
                     <h1 className="text-white ps-4">Create WebSIte</h1>
